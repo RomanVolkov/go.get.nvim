955484cc8228afe7d2f8d520f9472347e3fb7991
Roman Volkov <email@romanvolkov.ru>
2024-09-11 12:38:22 +0200

    increase batch size for deps.dev

--- index_builder/validations/deps.go
+++ index_builder/validations/deps.go
@@ -63,7 +63,7 @@ func ValidatePackageURLs(urls []string) map[string]bool {
 }
 
 func CleanupInvalidPackageURLs(uniqueURLs *map[string]bool) {
-	batchSize := 500
+	batchSize := 1000
 
 	uniquePackagesSlice := make([]string, 0)
 	for v := range *uniqueURLs {
